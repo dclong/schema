c027fd39511d9c1536017c3289d5b13513fae883
Ben Chuanlong Du <legendu.net@outlook.com>
2021-01-08 06:42:12 +0000

    add func dump_schema_db

--- schema/schema.py
+++ schema/schema.py
@@ -29,6 +29,45 @@ def _source_code_db(dbase, output):
         .parquet(f"{output}/{dbase}")
 
 
+def describe(table_name: str):
+    """Describe a table.
+    :param table_name: The name of the table to describe.
+    :param from_cache: Whether to read cached results (if available)
+    instead of querying the database.
+    :param to_cache: Whether to save SQL results to cache.
+    :return: A DataFrame containing schema of the table.
+    """
+    frame = spark.sql(f"DESCRIBE TABLE {table_name}").toPandas()
+    arr = np.where(frame.col_name.str.startswith("# "))[0]
+    if arr.size > 0:
+        frame = frame.iloc[:arr.min(), ].copy()
+    frame.col_name = frame.col_name.str.lower()
+    return frame
+
+
+def _dump_schema_db(self, dbase, output_dir: Path) -> None:
+    logger.info("Dumping schema of tables in the database {}", dbase)
+    output_dir = output_dir / dbase
+    output_dir.mkdir(parents=True, exist_ok=True)
+    tables = spark.sql(f"SHOW TABLES in {dbase}").toPandas()
+    for _, (db, table) in tqdm(tables[["database", "table"]].iterrows(), total=tables.shape[0]):
+        table = f"{db}.{table}"
+        schema = describe(table)
+        with (output_dir / f"{table}.txt").open("w") as fout:
+            fout.write(table + "\n")
+            for _, (col_name, data_type) in schema[["col_name", "data_type"]].iterrows(): 
+                fout.write(f"{col_name}    {data_type}\n")
+
+
+def dump_schema_db(self, dbs: Union[str, List[str]], output_dir: Union[str, Path] = ""):
+    if isinstance(dbs, str):
+        dbs = [dbs]
+    if isinstance(output_dir, str):
+        output_dir = Path(output_dir)
+    for db in dbs:
+        self._dump_schema_db(db, output_dir)
+
+
 def source_code_dbs(dbases: Iterable[str], output):
     """Get the source code of tables in databases.
     """
